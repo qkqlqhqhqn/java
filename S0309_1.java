39573f3a523a31f00be97e37ecab20629967345c
qkqlqhqhqn <daejung2603@dge.go.kr>
2026-04-21 10:07:18 +0900

    Add files via upload

---
+++ S0309_1.java
@@ -0,0 +1,10 @@
+package jumptojava;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class S0309_1 {
+	public static void main(String[] args) {
+		HashSet<String> set = new HashSet<>(Arrays.asList("H","e","l","l","o"));
+		System.out.println(set);
+	}
+}
---
+++ S0310_1.java
@@ -0,0 +1,15 @@
+package jumptojava;
+
+public class S0310_1 {
+	enum CoffeeType {
+		AMERICANO,
+		ICE_AMERICANO,
+		CAFE_LATTE
+	};
+	public static void main(String[] args) {
+		System.out.println(CoffeeType.AMERICANO);
+		System.out.println(CoffeeType.ICE_AMERICANO);
+		System.out.println(CoffeeType.CAFE_LATTE);
+	}
+
+}
